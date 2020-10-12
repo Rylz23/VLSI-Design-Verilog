c5a0372209cb459e5a8d91f61616c28644ff1500
Rylz23 <71660556+Rylz23@users.noreply.github.com>
2020-10-12 12:38:23 -0500

    Update bin_to_gray_behavioral.v

--- bin_to_gray_behavioral.v
+++ bin_to_gray_behavioral.v
@@ -1,7 +1,8 @@
 /* Author: Rylee Colleir
    Date Created: 09/21/2020
    Assignment: Lab 1
-   Description: Behavioral level code for a binary to gray code number converter
+   Description: Behavioral level code for a binary to gray code number converter.
+*/
 module bin_to_gray_behavioral (x1, x2, x3, x4, z1, z2, z3, z4);
 
 input x1, x2, x3, x4;
